4c1ac04249b2e7fc8fdd044add43b7a969b5dfa6
Aroun A. Dalawat <ray.aroun@gmail.com>
2021-09-05 16:58:42 -0700

    commit on 'main'

---
+++ dags/first_dag.py
@@ -0,0 +1,44 @@
+try:
+
+	from datetime import timedelta
+	from airflow import DAG
+	from airflow.operators.python_operator import PythonOperator
+	from datetime import datetime
+	import pandas as pd
+	# from datetime import datetime
+	print("All dag modules are okay")
+
+except Exception as e:
+	print("Error {}".format(e))
+
+
+def first_function_execute():
+	print("Hello world")
+	return "Hello World"
+
+
+with DAG(
+
+	dag_id = "first_dag",
+
+	schedule_interval = "@daily",
+
+	default_args = {
+
+		"owner" : "airflow",
+		"retries" : 1,
+		"retry_delay" : timedelta(minutes=5),
+		"start_date" : datetime(2021,1,1)
+	},
+	#telling it to skip all the previous runs
+	catchup = False) as f:
+
+	first_function_execute = PythonOperator(
+
+		task_id = "first_function_execute",
+		python_callable = first_function_execute)
+
+
+
+
+
